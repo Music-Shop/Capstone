1ae970e3ea073aa57b2457c6913a40fa32713fb7
Luc <ludan1214@gmail.com>
2021-04-14 00:49:16 -0500

    config for tomcat

--- src/main/java/com/hcl/MusicMelody/MusicMelodyApplication.java
+++ src/main/java/com/hcl/MusicMelody/MusicMelodyApplication.java
@@ -2,20 +2,26 @@ package com.hcl.MusicMelody;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
-public class MusicMelodyApplication {
+public class MusicMelodyApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicMelodyApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MusicMelodyApplication.class);
+    }
 }
