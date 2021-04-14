8999fa651f8215622867fef5a90ccbb1a14779b6
bijaya12345 <bijayakhanal652@yahoo.com>
2021-04-13 20:08:20 -0500

    commented billingService class

---
+++ bin/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+REPLACE INTO `roles` VALUES (1, 'ADMIN');
+REPLACE INTO `roles` VALUES (2, 'USER');
+
+-- replace into song (title,cost,duration,artist_id,album_id) values ('oh boi',23.20,'1:40',1,1),
+-- ('hello',2.12,'2:20',3,3),('GoodBye',12.99,'3:54',1,1),('Gold',9.99,'4:12',2,2),('Try',9.99,'4:25',2,2),
+-- ('Just my luck',5.99,'3:06',3,3),('Broken',7.99,'3:13',3,3),('follow',5.99,'2:49',2,2);
+
+-- replace into album(title) values ('Single'),('Mixtape 1'),('Test Pack');
